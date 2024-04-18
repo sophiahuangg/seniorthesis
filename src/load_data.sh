443c2e5de52d8e4f4e65aa230e7b9ec7a5f718d3
sophiahuangg <shuang24@students.claremontmckenna.edu>
2024-04-18 02:02:26 -0700

    Updating Shell Script

--- src/load_data.sh
+++ src/load_data.sh
@@ -1,12 +1,29 @@
 #!/bin/bash
 
-docker-compose exec pg psql -U user -d postgres -c "\COPY players FROM 'datasets/players.csv' WITH CSV HEADER;"
-docker-compose exec pg psql -U user -d postgres -c "\COPY teams FROM 'datasets/teams.csv' WITH CSV HEADER;"
-docker-compose exec pg psql -U user -d postgres -c "\COPY playercareerstats FROM 'datasets/playercareerstats.csv' WITH CSV HEADER;"
-docker-compose exec pg psql -U user -d postgres -c "\COPY games FROM 'datasets/games.csv' WITH CSV HEADER;"
-docker-compose exec pg psql -U user -d postgres -c "\COPY playerinfo FROM 'datasets/playerinfo.csv' WITH CSV HEADER;"
-docker-compose exec pg psql -U user -d postgres -c "\COPY regseasonboxscores FROM 'datasets/regseasonboxscores.csv' WITH CSV HEADER;"
-docker-compose exec pg psql -U user -d postgres -c "\COPY playoffboxscores FROM 'datasets/playoffboxscores.csv' WITH CSV HEADER;"
-docker-compose exec pg psql -U user -d postgres -c "\COPY playoffspbp FROM 'datasets/playoffspbp.csv' WITH CSV HEADER;"
-docker-compose exec pg psql -U user -d postgres -c "\COPY regseasonpbp FROM 'datasets/regseasonpbp.csv' WITH CSV HEADER;"
+# Loading All Data Except for Play By Play
 
+for data in datasets/*.csv; do
+    table="$(basename "$data" .csv)"
+    docker-compose exec pg psql -U user -d postgres -c "\COPY $table FROM '$data' WITH CSV HEADER;"
+done
+
+
+# Loading Regular Season Play by Play
+
+for z in datasets/*regpbp.zip; do
+    unzip -oj "$z" -d datasets/
+done
+
+for csv in datasets/*regSSN*.csv; do
+    docker-compose exec pg psql -U user -d postgres -c "\COPY regseasonpbp FROM '$csv' WITH CSV HEADER;"
+    rm "$csv"
+done
+
+# Loading Playpffs Play by Play
+
+unzip -oj datasets/playoffspbp.zip -d datasets/
+
+for csv in datasets/*playoffspbp.csv; do
+    docker-compose exec pg psql -U user -d postgres -c "\COPY playoffspbp FROM '$csv' WITH CSV HEADER;"
+    rm "$csv"
+done
